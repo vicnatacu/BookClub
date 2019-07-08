37dfe263b250ca15a36ba8a54ddddd126f08efb1
vicnatacu <vicki@mvbmb.com>
2019-07-09 09:06:06 +1000

    changes to check

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,9 +4,9 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
   config.serve_static_assets = true
-  config.assets.initialize_on_precompile = false
+  # config.assets.initialize_on_precompile = false
   config.assets.compile = true
-  config.assets.digest = true
+  # config.assets.digest = true
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
